80124eeb004ad9c26ff31d265c7c3b5ed8baf414
Alp <aakpinar@bu.edu>
2020-02-13 20:42:31 +0100

    Add choice of working node OS

--- mcmtest/submitter/submitter.py
+++ mcmtest/submitter/submitter.py
@@ -8,87 +8,95 @@ from mcmtest.lib.condor import condor_submit
 pjoin = os.path.join
 
 class Submitter:
-	'''
-	HTCondor submitter for a list of McMPrepIDs.
-	Provided the prepID list, adjusts the relevant settings
-	(proxy, file_transfer etc.) and submits all test jobs to HTCondor.
+    '''
+    HTCondor submitter for a list of McM prepIDs.
+    Provided the prepID list, adjusts the relevant settings
+    (proxy, file_transfer etc.) and submits all test jobs to HTCondor.
 
-	Example usage:
-	>>> sub = Submitter(prepid_list=<PrepID_list>)
-	
-	Get the submission settings for the requests:
-	>>> sub.submission_settings
+    Example usage:
+    >>> sub = Submitter(prepid_list=<PrepID_list>)
+    
+    Get the submission settings for the requests:
+    >>> sub.submission_settings
 
-	Submit all jobs to HTCondor:
-	>>> sub.submit()
-	'''
-	def __init__(self, prepid_list):
-		'''Initializer function for the Submitter class.
-		   Initializes the submission settings for HTCondor.
+    Submit all jobs to HTCondor:
+    >>> sub.submit()
+    '''
+    def __init__(self, prepid_list, node_os='centos7'):
+        '''Initializer function for the Submitter class.
+           Initializes the submission settings for HTCondor.
 
-		   ================
-		   PARAMETERS
-		   ================
-		   prepid_list : The list of McM PrepIDs that are being tested.
-		'''
-		self.prepid_list = prepid_list
-	
-		executable = '/afs/cern.ch/user/a/aakpinar/mcmtest/mcmtest/execute/run_test.sh'
-	
-		# Initialize submission settings 
-		# for HTCondor
-		self.submission_settings = {
-			'universe' : 'vanilla',
-			'executable' : executable, 
-			'should_transfer_files' : 'YES',
-			'transfer_output_files' : '""',
-			# Run jobs only on SLC6 
-			'requirements' : '(OpSysAndVer =?= "SLCern6")',
-			# Maximum run-time: 2 hours
-			'+JobFlavour'  : 'longlunch' 
-		}
-		# Copy the VOMS proxy file to AFS
-		# and get the new path to it	
-		proxy_path = copy_proxy()
-		self.submission_settings['proxy_path'] = proxy_path
-	
-		# Transfer the VOMS proxy file to HTCondor
-		self.submission_settings['transfer_input_files'] = proxy_path
+           ================
+           PARAMETERS
+           ================
+           prepid_list : The list of McM PrepIDs that are being tested.
+           node_os     : The desired operating system in the work nodes.
+                         The default OS is CentOS 7 (defualt OS HTCondor submits to), 
+                         but Submitter also accepts Scientific Linux 6 (node_os=slc6). 
+        '''
+        self.prepid_list = prepid_list
+    
+        executable = '/afs/cern.ch/user/a/aakpinar/mcmtest/mcmtest/execute/run_test.sh'
+    
+        # Initialize submission settings 
+        # for HTCondor
+        self.submission_settings = {
+            'universe' : 'vanilla',
+            'executable' : executable, 
+            'should_transfer_files' : 'YES',
+            'transfer_output_files' : '""',
+            # Maximum run-time: 2 hours
+            '+JobFlavour'  : 'longlunch' 
+        }
+        # If SLC6 OS is specified, add it to submission settings
+        if node_os == 'slc6':
+            self.submission_settings['requirements'] = '(OpSysAndVer =?= "SLCern6")'
+        
+        if node_os not in ['slc6', 'centos7']:
+            raise ValueError('Invalid value for node_os: Use centos7 or slc6.')
 
-	def submit(self):	
-		'''Submit the test jobs for all prepIDs to HTCondor.'''
-		for prepid in self.prepid_list:
-			# Set the correct arguments for the executable
-			arg_list = ['$(proxy_path)', prepid]
-			args = ' '.join(arg_list)		
-			self.submission_settings['arguments'] = args
+        # Copy the VOMS proxy file to AFS
+        # and get the new path to it    
+        proxy_path = copy_proxy()
+        self.submission_settings['proxy_path'] = proxy_path
+    
+        # Transfer the VOMS proxy file to HTCondor
+        self.submission_settings['transfer_input_files'] = proxy_path
 
-			# Set output log files
-			outdir = mcmtest_path('output')
-			if not os.path.exists(outdir):
-				os.makedirs(outdir)
-			output_file = pjoin(outdir, 'out_{0}.txt'.format(prepid))
-			log_file = pjoin(outdir, 'log_{0}.txt'.format(prepid))
-			err_file = pjoin(outdir, 'err_{0}.txt'.format(prepid))
+    def submit(self):    
+        '''Submit the test jobs for all prepIDs to HTCondor.'''
+        for prepid in self.prepid_list:
+            # Set the correct arguments for the executable
+            arg_list = ['$(proxy_path)', prepid]
+            args = ' '.join(arg_list)        
+            self.submission_settings['arguments'] = args
 
-			self.submission_settings['output'] = output_file
-			self.submission_settings['log'] = log_file
-			self.submission_settings['error'] = err_file
+            # Set output log files
+            outdir = mcmtest_path('output')
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+            output_file = pjoin(outdir, 'out_{0}.txt'.format(prepid))
+            log_file = pjoin(outdir, 'log_{0}.txt'.format(prepid))
+            err_file = pjoin(outdir, 'err_{0}.txt'.format(prepid))
 
-			sub = htcondor.Submit(self.submission_settings)
+            self.submission_settings['output'] = output_file
+            self.submission_settings['log'] = log_file
+            self.submission_settings['error'] = err_file
 
-			# Write the job file to submit
-			jobfiledir = mcmtest_path('job_files') 
-			if not os.path.exists(jobfiledir):
-				os.makedirs(jobfiledir)
+            sub = htcondor.Submit(self.submission_settings)
 
-			jobfile = pjoin(jobfiledir, 'job_{0}.jdl'.format(prepid))
-			with open(jobfile, 'w+') as f:
-				f.write(str(sub))
-				f.write('\nqueue 1\n')
+            # Write the job file to submit
+            jobfiledir = mcmtest_path('job_files') 
+            if not os.path.exists(jobfiledir):
+                os.makedirs(jobfiledir)
 
-			# Submit the job
-			jobid = condor_submit(jobfile)
-			print('Submitted job: {0}, Job ID: {1}'.format(prepid, jobid))
+            jobfile = pjoin(jobfiledir, 'job_{0}.jdl'.format(prepid))
+            with open(jobfile, 'w+') as f:
+                f.write(str(sub))
+                f.write('\nqueue 1\n')
+
+            # Submit the job
+            jobid = condor_submit(jobfile)
+            print('Submitted job: {0}, Job ID: {1}'.format(prepid, jobid))
 
 
